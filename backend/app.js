1adaae6f406e42743cc07c4beb018843f0b4840f
ethanrestrada <etanr25@gmail.com>
2025-11-20 22:57:18 -0600

    cambio de app.js backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+// require("dotenv").config();
 
 const sequelize = require("./db/db");
 const ClienteRoutes = require("./routes/cliente.routes");
