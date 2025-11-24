bc83460f4e5a9848af5b811f76fc2910a058d634
ethanrestrada <etanr25@gmail.com>
2025-11-24 10:13:10 -0600

    prueba pull dokploy 13

--- frontend/src/components/CrudForm.jsx
+++ frontend/src/components/CrudForm.jsx
@@ -74,7 +74,7 @@ export default function CrudForm({
   return (
     <div>
       <h3>{dataToEdit ? "Editar" : "Agregar"}</h3>
-      <span>cambio aqui 15</span>
+      <span>cambio aqui 16</span>
       <form onSubmit={handleSubmit}>
         <FormControl
           icon="fa-solid fa-user"
